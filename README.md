394704198b545d92167bb14689ef7df9de024168
Lukas Thim <kastrullkastaren@hotmail.com>
2017-03-09 16:36:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# RedditBot
+<<<<<<< HEAD
+Reddit Bot
+=======
+A good Reddit bot
+>>>>>>> 80da9e2b8d757d888b53b7e91769b248de70691f
---
+++ RedditBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RedditBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
